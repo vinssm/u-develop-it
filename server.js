34416093d6eb80f293c2932e3bb5c9f6a7589268
Vinay Vallabhaneni <vinssmedia@gmail.com>
2022-01-26 21:52:59 -0500

    db

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3009;
@@ -7,6 +9,18 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Mysql',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 // app.get('/', (req, res) => {
 //     res.json({
@@ -14,6 +28,10 @@ app.use(express.json());
 //     });
 //   });
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+  
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
